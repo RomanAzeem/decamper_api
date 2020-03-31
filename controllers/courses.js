70a82e0e802aa15943b2655d86e8226e41c37e4f
RomanAzeem <romanazeem007@gmail.com>
2020-03-31 09:20:01 +0500

    models controller routes for bootcamp/courses

---
+++ controllers/courses.js
@@ -0,0 +1,39 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
+
+// @desc      Get courses
+// @route     GET /api/v1/courses
+// @route     GET /api/v1/bootcamps/:bootcampId/courses
+// @access    Public
+exports.getCourses = asyncHandler(async (req, res, next) => {
+  const courses = await Course.find();
+
+  return res.status(200).json({
+    success: true,
+    count: courses.length,
+    data: courses
+  });
+});
+
+// @desc      Get single course
+// @route     GET /api/v1/courses/:id
+// @access    Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description'
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course
+  });
+});
